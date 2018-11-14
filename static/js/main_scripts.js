3ff1b888470a99cd7d2705e75423667e917eefd6
Tsanko <tsankohadzhiev@gmail.com>
2018-11-14 12:17:32 +0100

    remove testing url

--- static/js/main_scripts.js
+++ static/js/main_scripts.js
@@ -65,7 +65,7 @@ App.init = function () {
 	$(".importar").addEventListener("click", function () {
 		var form = document.createElement('form');
 		form.setAttribute('method', 'post');
-		form.setAttribute('action', 'http://google.com');
+		form.setAttribute('action', '/');
 		form.style.display = 'hidden';
 		document.body.appendChild(form)
 		form.submit();
